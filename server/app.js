505da80baa2c6aa6d7d07d44ea854e63b70a3cfe
adir <adir@gmail.com>
2024-08-30 18:48:29 +0300

    Enable swagger API

--- server/app.js
+++ server/app.js
@@ -3,6 +3,9 @@ import {mongoConnect} from './config/db.js';
 import {authRoutes} from './routes/authRoutes.js';
 import {productRoutes} from './routes/productRoutes.js';
 import mongoose from "mongoose";
+import swaggerUi from "swagger-ui-express"
+import {swaggerSpec} from "./swagger.js"
+
 
 const app = express();
 
@@ -12,8 +15,15 @@ mongoose.connection.on('disconnected', () => {
 mongoConnect();
 
 app.use(express.json());
+
 app.use('/api/auth', authRoutes);
 app.use('/api/product', productRoutes);
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+    console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
+
+});
