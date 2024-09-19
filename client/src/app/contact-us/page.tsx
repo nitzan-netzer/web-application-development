684b0131bd3a130d6d84e8b44ba90c2eb6b0a562
nitzan <nitzan.netzer1@cs.colman.ac.il>
2024-09-19 11:31:08 +0300

    add ContactUs

---
+++ client/src/app/contact-us/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ContactUs from '@/srccomponents/ContactUs'
+
+export default function ContactUsApp() {
+    return (
+        <div className="ContactUsApp">
+            <main>
+                <ContactUs />
+            </main>
+        </div>
+    )
+}
