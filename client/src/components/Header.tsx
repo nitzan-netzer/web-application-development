2f590b042fd2e78a33b8d195020ad73705c96e3b
nitzan <nitzan.netzer1@gmail.com>
2024-09-29 17:52:37 +0300

    fix herf

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,24 +4,24 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
-import React from 'react'
+import React from 'react';
 import { useRouter } from 'next/navigation';
-
-
-
 
 function Header() {
   const router = useRouter();
+
   const handleClickRegister = () => {
-    router.push('/register');
-  }
+    router.push('/register'); // Use router.push instead of window.location.href
+  };
+
   const handleClickLogin = () => {
     router.push('/login');
-  }
+  };
+
   return (
     <Navbar fixed='top' expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand href="/">
           <img
             src={'/logo.jpeg'}
             width="60"
@@ -29,11 +29,11 @@ function Header() {
             className="d-inline-block align-top"
             alt="רגל 2 logo"
           />
-          <Button onClick={handleClickRegister} variant="outline-success"
+          <Button href="/register" variant="outline-success"
             style={{ marginLeft: '20px', position: 'relative', top: '10px' }}>הרשמה</Button>
-           <Button onClick={handleClickLogin} variant="outline-success"
+          <Button href="/login" variant="outline-success"
             style={{ marginLeft: '20px', position: 'relative', top: '10px' }}>התחברות</Button>
-          {/* <a href="/target-page" className="ms-2">
+          <a href="/cart" className="ms-2">
             <img
               src={'/shopping-cart.png'}
               width="30"
@@ -42,23 +42,22 @@ function Header() {
               alt="Icon"
               style={{
                 position: 'relative',
-                top: '10px',  // Moves the image down by 10 pixels
-                left: '5px',  // Moves the image to the right by 5 pixels
+                top: '10px',
+                left: '10px',
                 cursor: 'pointer'
               }}
             />
-          </a> */}
+          </a>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
-            className="ms-auto flex-row-reverse" // Aligns the Nav links to the right
+            className="ms-auto flex-row-reverse"
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link href="/home" className="mx-3">דף הבית</Nav.Link>
+            <Nav.Link href="/" className="mx-3">דף הבית</Nav.Link>
             <Nav.Link href="/products" className="mx-3">ההמוצרים שלנו</Nav.Link>
-            <Nav.Link href="/cart" className="mx-3">סל קניות</Nav.Link>
             <Nav.Link href="/about" className="mx-3">אז מי אנחנו</Nav.Link>
             <Nav.Link href="/contact-us" className="mx-3">צרו קשר</Nav.Link>
             <Nav.Link href="/policy" className="mx-3">תקנון</Nav.Link>
@@ -66,7 +65,7 @@ function Header() {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
+  );
 }
 
-export default Header
+export default Header;
