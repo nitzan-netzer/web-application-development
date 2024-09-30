519c9f84ad85c55f8fe9a432b977ad3ee24f60f9
adir <adir@gmail.com>
2024-09-30 10:05:37 +0300

    Setup documentation

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # web-application-development
+
+## Setup Application
+
+To setup and deploy the development environment, follow the next steps:
+
+### Db Infrastructure
+
+You should take care to install and initialize mongodb running localhost...
+
+### Server & client
+
+First open 2 Terminals
+
+First terminal for client
+
+```bash
+~/code/web-application-development > cd client
+~/code/web-application-development/client > npm i
+~/code/web-application-development/client > npm run dev
+```
+
+Second terminal for server
+
+```bash
+~/code/web-application-development > cd server
+~/code/web-application-development/server > npm i
+~/code/web-application-development/server > npm run dev
+```
+
+That it your application is up now.
--- server/app.js
+++ server/app.js
@@ -2,14 +2,13 @@ import express from 'express';
 import {mongoConnect} from './config/db.js';
 import {authRoutes} from './routes/authRoutes.js';
 import {productRoutes} from './routes/productRoutes.js';
-import {purchaseRoutes} from "./routes/purchaseRoutes.js";
 import mongoose from "mongoose";
 import swaggerUi from "swagger-ui-express"
 import swaggerDoc from "./defenitions/swagger.json" assert { type: "json" }
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import {createInitialData} from "../server/utils/initialData.js";
+import { adminRoutes } from './routes/adminRoutes.js';
 
 const app = express();
 const corsOptions = {
@@ -20,24 +19,16 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-mongoose.connection.on('error', () => {
-    mongoConnect();
-});
-mongoose.connection.on('connected', () => {
-    console.log('Connected to MongoDB');
-});
 mongoose.connection.on('disconnected', () => {
     mongoConnect();
 });
 mongoConnect();
-await createInitialData();
-
 
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/product', productRoutes);
-app.use('/api/purchase', purchaseRoutes);
+app.use('/api/admin', adminRoutes)
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -45,9 +36,9 @@ app.use('/api/public', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
-
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
     console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
+
 });
