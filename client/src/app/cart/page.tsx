69e8130700e698340d186fd505cd8a15e051a0af
OrCohenNaznin <ornaznin@gmail.com>
2024-10-05 13:05:58 +0300

    connection between cart and postPurches

--- client/src/app/cart/page.tsx
+++ client/src/app/cart/page.tsx
@@ -2,6 +2,7 @@
 import Cart from '@/srccomponents/Cart';
 
 export default async function cartApp() {
+    // const products = localStorage getItem('products');
     return (
         <div className="cartApp">
             <Cart />
