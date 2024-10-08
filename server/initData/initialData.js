5ccfb1f2c142afd34e30dedd2695c42d8a02a0e9
Dana Cohen <dana.co@moonactive.com>
2024-10-08 17:06:17 +0300

    product name

--- server/initData/initialData.js
+++ server/initData/initialData.js
@@ -8,16 +8,16 @@ export async function createInitialData() {
 }
 
 async function createProductInitialData() {
-    await createProducts("Jewelry", "Or Cohen Naznin", "available", JEWLS_PICS.slice(0,4), 0);
-    await createProducts("Jewelry", "Or Cohen Naznin", "soldOut", JEWLS_PICS.slice(5,9), 5);
-    await createProducts("Clothes", "Rotem Raz", "available", CLOTHING_PICS.slice(0,4), 0);
-    await createProducts("Clothes", "Rotem Raz", "soldOut", CLOTHING_PICS.slice(5,9), 5);
-    await createProducts("Toys", "Dana Cohen", "available", TOYS_PICS.slice(0,4), 0);
-    await createProducts("Toys", "Dana Cohen", "soldOut", TOYS_PICS.slice(5,9), 5);
-    await createProducts("Sport Products", "Adir Itzhaki", "available", SPORT_PICS.slice(0,1), 0);
-    await createProducts("Sport Products", "Adir Itzhaki", "soldOut", SPORT_PICS.slice(2,3), 2);
-    await createProducts("Electrical Products", "Einav Haimovich", "available", SPORT_PICS.slice(0,2), 0);
-    await createProducts("Electrical Products", "Einav Haimovich", "soldOut", SPORT_PICS.slice(3,5), 3);
+    await createProducts("תכשיטים", "Or Cohen Naznin", "available", JEWLS_PICS.slice(0,4), 0);
+    await createProducts("תכשיטים", "Or Cohen Naznin", "soldOut", JEWLS_PICS.slice(5,9), 5);
+    await createProducts("ביגוד ואופנה", "Rotem Raz", "available", CLOTHING_PICS.slice(0,4), 0);
+    await createProducts("ביגוד ואופנה", "Rotem Raz", "soldOut", CLOTHING_PICS.slice(5,9), 5);
+    await createProducts("משחקים וצעצועים", "Dana Cohen", "available", TOYS_PICS.slice(0,4), 0);
+    await createProducts("משחקים וצעצועים", "Dana Cohen", "soldOut", TOYS_PICS.slice(5,9), 5);
+    await createProducts("אביזרי ספורט", "Adir Itzhaki", "available", SPORT_PICS.slice(0,1), 0);
+    await createProducts("אביזרי ספורט", "Adir Itzhaki", "soldOut", SPORT_PICS.slice(2,3), 2);
+    await createProducts("מוצרי חשמל", "Einav Haimovich", "available", SPORT_PICS.slice(0,2), 0);
+    await createProducts("מוצרי חשמל", "Einav Haimovich", "soldOut", SPORT_PICS.slice(3,5), 3);
 }
 
 async function createProducts(category, sellerName, status, photosArray, locationIndex){
